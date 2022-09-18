53bcdb2a9449ad45ffeed26e40a1c92141b187d8
AkmalBots <112285492+AkmalBots@users.noreply.github.com>
2022-09-18 16:40:43 +0700

    Update i-donasi.js

--- plugins/i-donasi.js
+++ plugins/i-donasi.js
@@ -8,13 +8,13 @@ let handler = async (m, { conn, usedPrefix }) => {
       extendedTextMessage: {
       text: `
 ╭─「 Donasi • Dana 」
-│ • im3 [085822347348]
-│ • axis  [083843192208]
-│ • Dana  [085822347348]
-│ • Saweria  [https://saweria.co/arullofc]
+│ • Telkomsel [081269734000]
+│ • Telkomsel [081370599523]
+│ • Dana  [081269734000]
+│ • Saweria  [https://saweria.co/Holer]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/6285822347348
+│ > Ingin donasi? Wa.me/6281269734000
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
